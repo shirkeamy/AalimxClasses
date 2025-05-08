f2f955c927d596521aff44f7105262385ed9b87f
Amol Shirke <shirkeamy@gmail.com>
2025-05-08 09:17:41 +0530

    Oct2024Batch | Amol | Session 2 - Typescript Hello World

--- Oct24/Typescript/example1.js
+++ Oct24/Typescript/example1.js
@@ -1,2 +1,5 @@
 var welcome = "Welcome to Typescript";
 console.log(welcome);
+var heading = document.createElement("h1");
+heading.textContent = "Hello, World!, Welcome to Ts";
+document.body.appendChild(heading);
--- Oct24/Typescript/example1.ts
+++ Oct24/Typescript/example1.ts
@@ -1,3 +1,9 @@
 const welcome: string = "Welcome to Typescript";
 
 console.log(welcome);
+
+
+let heading = document.createElement("h1");
+heading.textContent = "Hello, World!, Welcome to Ts";
+
+document.body.appendChild(heading);
