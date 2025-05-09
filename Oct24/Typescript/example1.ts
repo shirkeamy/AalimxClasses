df64eba28808e544944123671980eabcad021a2c
Amol Shirke <shirkeamy@gmail.com>
2025-05-09 15:31:06 +0530

    Oct2024Batch | Amol | Session 3 - Introduction to TS Types

--- Oct24/Typescript/example1.ts
+++ Oct24/Typescript/example1.ts
@@ -7,3 +7,26 @@ let heading = document.createElement("h1");
 heading.textContent = "Hello, World!, Welcome to Ts";
 
 document.body.appendChild(heading);
+
+let variableName: string | undefined | null;
+console.log(variableName);
+
+let varName: string;
+varName = "any text";
+varName = "any text 2";
+
+const varName1: string = "text 1";
+// varName1 = ""
+
+const varName2: boolean = true;
+const varName3: number = 1;
+const varName4: bigint = 1n;
+const todaysDate: Date = new Date();
+
+const person: {
+    name: string,
+    age: number
+} = {
+    name: "Dan",
+    age: 32
+};
