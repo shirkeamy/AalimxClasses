d45fd4003fec2fab8817be30edf8ede424e84087
Amol Shirke <shirkeamy@gmail.com>
2025-05-31 09:07:58 +0530

    Dec2024Batch | Amol | Session 13 - JSX and Fragment tag

--- Dec24/React/may-app-1/src/App.tsx
+++ Dec24/React/may-app-1/src/App.tsx
@@ -4,11 +4,24 @@ import './App.css';
 
 
 function App() {
+
+  const userName: string = "John";
+  const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   return (
     <>
-      <h1 className='text-success'>
-        Welsome React Ts
+      <h1 key={"welcome-msg"} className='text-success'>
+        Welcome, {userName}
       </h1>
+      <ul key={"numbers"}>
+        {
+          numbers.map((num: number, index: number) => {
+            return (
+              <li key={`number-${index}`}>{num}</li>
+            )
+          })
+        }
+      </ul>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptatum modi, pariatur totam id eius beatae facilis eligendi blanditiis tempora adipisci enim consectetur maxime doloremque dolor nobis commodi accusantium quaerat?
       </p>
