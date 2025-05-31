7e8f1b4c1474d684936b6e1b60c89779e57342fa
Amol Shirke <shirkeamy@gmail.com>
2025-05-31 08:17:09 +0530

    Dec2024Batch | Amol | Session 12 - Create second react application using Typescript template

---
+++ Dec24/React/may-app-1/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-success'>
+        Welsome React Ts
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptatum modi, pariatur totam id eius beatae facilis eligendi blanditiis tempora adipisci enim consectetur maxime doloremque dolor nobis commodi accusantium quaerat?
+      </p>
+    </>
+  );
+}
+
+export default App;
