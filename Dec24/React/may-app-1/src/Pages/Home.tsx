02bd972984377a861bb1b9de784f3368664cb72d
Amol Shirke <shirkeamy@gmail.com>
2025-06-11 08:59:15 +0530

    Dec2024Batch | Amol | Session 16 - React useState hook

--- Dec24/React/may-app-1/src/Pages/Home.tsx
+++ Dec24/React/may-app-1/src/Pages/Home.tsx
@@ -1,8 +1,21 @@
+import { useState } from "react";
+
 const Home: React.FC = () => {
+
+  // let count = 0;
+  const [count, setCount] = useState<number>(0);
+
   return (
     <div className="home">
       <h1>Welcome to the Home Page</h1>
       <p>This is the main page of our application.</p>
+      <h2>Counter: {count}</h2>
+      <input
+        type="button"
+        value="Add"
+        className="btn btn-primary btn-sm"
+        onClick={() => { setCount(count + 1) }}
+      />
     </div>
   );
 }
