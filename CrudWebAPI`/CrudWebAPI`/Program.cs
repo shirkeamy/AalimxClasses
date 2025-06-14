62c9ca89e1bff7273a40e20dfa0600dc6cbbf84d
Amol Shirke <shirkeamy@gmail.com>
2025-06-14 11:50:49 +0530

    Enable cors in application

--- CrudWebAPI`/CrudWebAPI`/Program.cs
+++ CrudWebAPI`/CrudWebAPI`/Program.cs
@@ -10,6 +10,17 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<DbHelper>();
 
+// Enable CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +29,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use CORS
+app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
