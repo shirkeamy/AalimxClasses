20f7b480af145b539340aeb946e713f327e2a1be
Amol Shirke <shirkeamy@gmail.com>
2025-07-10 08:52:36 +0530

    Feb25Batch | Amol | Session 2 - Typescript Hello world and ways to run ts file

--- AalimEx_Feb25/typescript/app.js
+++ AalimEx_Feb25/typescript/app.js
@@ -1,2 +1,11 @@
-var welcome = "4564";
+var welcome = "welcome to TypeScript from console";
 console.log(welcome);
+var heading = document.createElement("h1");
+heading.textContent = "Welcome to TypeScript";
+document.body.appendChild(heading);
+function addition(num1, num2) {
+    return num1 + num2;
+}
+console.log(addition(50, 20));
+console.log(addition(50, 50));
+//addition("10", "20");
--- AalimEx_Feb25/typescript/app.ts
+++ AalimEx_Feb25/typescript/app.ts
@@ -1,2 +1,14 @@
-let welcome: string = "4564";
+let welcome: string = "welcome to TypeScript from console";
 console.log(welcome);
+
+let heading = document.createElement("h1");
+heading.textContent = "Welcome to TypeScript";
+document.body.appendChild(heading);
+
+function addition(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+console.log(addition(50, 20));
+console.log(addition(50, 50));
+//addition("10", "20");
