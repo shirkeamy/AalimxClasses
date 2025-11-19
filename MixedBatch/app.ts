152f95e6635d642ba1f7209a55a42c4019d005bb
Amol Shirke <shirkeamy@gmail.com>
2025-11-19 20:52:15 +0530

    Mixed Batch | Amol | Session 1 - How to run ts files

---
+++ MixedBatch/app.ts
@@ -0,0 +1,18 @@
+function Add(a: number, b: number) {
+    return a + b;
+}
+
+console.log("The sum of 5 and 3 is:", Add(5, 3));
+// console.log("The sum of 5 and 3 is:", Add("5", 3)); // Argument of type 'string' is not assignable to parameter of type 'number'.
+
+// To rune ts file use command: tsc aap.ts
+
+function Add1(a, b) {
+    return a + b;
+}
+
+console.log("The sum of 5 and 3 is:", Add(5, 3));
+console.log("The sum of 5 and 3 is:", Add(50, 3));
+
+// to run ts in watch mode use command: tsc app.ts --watch
+// to run ts in watch mode use command: tsc -w app.ts
---
+++ MixedBatch/demo.js
@@ -0,0 +1,10 @@
+function Addition(a, b) {
+    return a + b;
+}
+
+// console.log() // print information to the console
+// console.error() // print error to the console
+// console.warn() // print warning to the console
+
+console.log("The sum of 5 and 3 is:", Addition(5, 3));
+console.warn("The sum of 5 and 3 is:", Addition("asda", "3"));
