c61dea6ccce3b7f770129ad01b0a89f996208acd
Amol Shirke <shirkeamy@gmail.com>
2025-12-05 20:45:28 +0530

    MixedBatch | Amol | React session2 - JSX and Fragment in React

--- MixedBatch/React/react-ts-application/src/App.tsx
+++ MixedBatch/React/react-ts-application/src/App.tsx
@@ -3,23 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let username: string = "Guest";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <>
+      <div className="App">
+        <h1>{username}</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          {
+            5 + 6
+          }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </div>
+      <div className="App-header">
+        Lorem ipsum dolor sit amet.
+      </div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dolore cumque obcaecati et ut, vero nulla earum doloribus nam magni enim ea? Sint delectus reprehenderit dignissimos recusandae facere eligendi odit!
+      </p>
+      <h1>
+        End of the content
+      </h1>
+    </>
   );
 }
 
