5f351a25cb22d57ed6d88a56fd78c71bf7ba6885
Amol Shirke <shirkeamy@gmail.com>
2025-12-16 20:16:43 +0530

    Remove unused CSS and SVG files to clean up the project

--- MixedBatch/React/react-ts-application/src/App.tsx
+++ MixedBatch/React/react-ts-application/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Home from './Pages/Home';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
 import About from './Pages/About';
